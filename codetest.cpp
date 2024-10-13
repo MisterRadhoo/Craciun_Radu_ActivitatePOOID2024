e2bfbd02a5022dc4846b9a8ec9142a3a615b67ed
MisterRadhoo <105071500+MisterRadhoo@users.noreply.github.com>
2024-10-13 18:24:53 +0300

    Add files via upload

---
+++ codetest.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+// create queue of strings names;
+	queue<string> names;
+
+	// push names in the queue;
+	names.push("Craciun");
+	names.push("Radu");
+	names.push("Mihai");
+
+	cout << "Queue with names: ";
+
+	// print the names on display;
+	while (!names = NULL) {
+		cout << names.front() << ", ";
+
+	}
+	cout << endl;
+	return 0;
+
+}
+
