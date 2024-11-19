26ba5b2da205f256c5f6d59d37a8dfee768a86b1
MisterRadhoo <105071500+MisterRadhoo@users.noreply.github.com>
2024-11-19 23:09:07 +0200

    Update tema1.cpp

--- ProjectASE/tema1.cpp
+++ ProjectASE/tema1.cpp
@@ -141,8 +141,8 @@ int main()
     cout << autobuz3 << endl;
     cout << autobuz4 << endl;
     cout << endl;
-    cout << "Constructorul de copiere chemat !!! \n"
-         << autobuz5 << endl;
+    cout << "Constructorul de copiere chemat !!! \n";
+    cout << autobuz5 << endl;
     autobuz5.setCapacitate(65);
     autobuz5.setPersoaneImbarcate(53);
     cout << "Capacitatea autobuzului este de: ->> " << autobuz5.getCapacitate() << " locuri pe scaune" << endl;
